7bfd45cf9abf3e98e0c8d1a015eb6fd1668b0c40
ShreyaMahajan67 <165214661+ShreyaMahajan67@users.noreply.github.com>
2025-11-05 20:34:18 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ window._config = {
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://99c1y8rnz1.execute-api.eu-north-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
