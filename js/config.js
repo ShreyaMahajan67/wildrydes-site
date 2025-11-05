a944661dcb84b75729c60820445e347a45d4bdbf
ShreyaMahajan67 <165214661+ShreyaMahajan67@users.noreply.github.com>
2025-11-05 19:34:43 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_E2gRQ72g7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'c3m4vmc3cads0eo7u2mpp5l4a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
